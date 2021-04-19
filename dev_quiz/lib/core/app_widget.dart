358474719006886e5fa22fb204a5d79c62ebc394
pedrocganem <pedrocastilho.ganemg@gmail.com>
2021-04-19 09:11:19 -0300

    adds support for web, macOS and adds assets

---
+++ dev_quiz/lib/core/app_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
